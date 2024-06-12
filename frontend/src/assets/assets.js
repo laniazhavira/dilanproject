7be29a15380a069b82e586df0c8d4e65202c63be
kodokaudi <audidiandra31@gmail.com>
2024-06-12 14:16:03 +0700

    First Commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,350 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Bread",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Pastry",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Pie & Tart",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Donut",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Pizza",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Cookies",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Pasta",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Drink",
+        menu_image: menu_8
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Baguette",
+        image: food_1,
+        price: 45,
+        description: "Enjoy the classic taste of France with our Baguette. This long, slender loaf features a crispy crust and a soft, airy interior, perfect for sandwiches.",
+        category: "Bread"
+    },
+    {
+        _id: "2",
+        name: "Wheat Bread",
+        image: food_2,
+        price: 35,
+        description: "Our Whole Wheat Bread is made from nutritious whole grain wheat flour, offering a rich, slightly sweet flavor and a dense, hearty texture.",
+        category: "Bread"
+    }, {
+        _id: "3",
+        name: "Focaccia",
+        image: food_3,
+        price: 40,
+        description: "Indulge in the savory goodness of our Focaccia. This Italian flatbread is seasoned with olive oil, salt, and rosemary.",
+        category: "Bread"
+    }, {
+        _id: "4",
+        name: "Ciabatta",
+        image: food_4,
+        price: 40,
+        description: "Experience the authentic taste of Italy with our Ciabatta. This flat, broad bread boasts a crispy crust and a chewy.",
+        category: "Bread"
+    }, {
+        _id: "5",
+        name: "Croissant",
+        image: food_5,
+        price: 30,
+        description: "Our Croissant is a classic French pastry, known for its buttery, flaky layers and golden-brown crust. Perfect for breakfast or a light snack, it pairs wonderfully with coffee or tea.",
+        category: "Pastry"
+    }, {
+        _id: "6",
+        name: "Éclair",
+        image: food_6,
+        price: 25,
+        description: "Indulge in our Éclair, a light choux pastry filled with creamy custard and topped with a smooth chocolate glaze. It's a delightful treat that melts in your mouth with every bite.",
+        category: "Pastry"
+    }, {
+        _id: "7",
+        name: "Cromboloni",
+        image: food_7,
+        price: 30,
+        description: "Enjoy our Cromboloni, a delicious pastry filled with rich cream or custard, often dusted with powdered sugar. It's a perfect indulgence for any time of the day.",
+        category: "Pastry"
+    }, {
+        _id: "8",
+        name: "Pain au chocolat",
+        image: food_8,
+        price: 35,
+        description: "Savor our Pain au Chocolat, a flaky and buttery pastry filled with rich, dark chocolate. Ideal for breakfast or as a decadent snack.",
+        category: "Pastry"
+    }, {
+        _id: "9",
+        name: "Crostata",
+        image: food_9,
+        price: 20,
+        description: "Our Crostata is a rustic Italian tart featuring a buttery, flaky crust filled with sweet fruit preserves or fresh fruit. This charming dessert is perfect for enjoying with a cup of coffee or as a delightful finish to any meal.",
+        category: "Pie & Tart"
+    }, {
+        _id: "10",
+        name: "Apple Pie",
+        image: food_10,
+        price: 23,
+        description: "Our Apple Pie features a flaky, golden crust filled with sweet, spiced apples. This classic dessert is perfect for any occasion, offering a taste of home in every bite.",
+        category: "Pie & Tart"
+    }, {
+        _id: "11",
+        name: "Lemon Meringue Pie",
+        image: food_11,
+        price: 23,
+        description: "Indulge in our Lemon Meringue Pie, a tangy lemon filling topped with a fluffy, toasted meringue. This pie is a refreshing and light dessert, perfect for ending any meal on a high note.",
+        category: "Pie & Tart"
+    }, {
+        _id: "12",
+        name: "Fruit Tart",
+        image: food_12,
+        price: 20,
+        description: "Our Fruit Tart is a delightful treat, featuring a buttery crust filled with creamy custard and topped with an assortment of fresh, seasonal fruits. ",
+        category: "Pie & Tart"
+    },
+    {
+        _id: "13",
+        name: "Maple Bar Donut",
+        image: food_13,
+        price: 20,
+        description: "Enjoy the rich and comforting flavor of our Maple Bar Donut. This rectangular-shaped donut is topped with a sweet maple glaze, offering a perfect blend of flavors that is sure to satisfy your cravings.",
+        category: "Donut"
+    },
+    {
+        _id: "14",
+        name: "Cinnamon Donut",
+        image: food_14,
+        price: 18,
+        description: "Savor the warmth and sweetness of our Cinnamon Donut. This donut is coated in a blend of cinnamon and sugar, providing a perfect balance of spicy and sweet flavors that make for a cozy and delicious treat.",
+        category: "Donut"
+    }, {
+        _id: "15",
+        name: "Custard-Filled Donut",
+        image: food_15,
+        price: 18,
+        description: "Delight in the creamy goodness of our Custard-Filled Donut. This donut is generously filled with smooth, velvety custard and topped with a dusting of powdered sugar.",
+        category: "Donut"
+    }, {
+        _id: "16",
+        name: "Sprinkled Donut",
+        image: food_16,
+        price: 15,
+        description: "Add a touch of fun to your day with our Sprinkled Donut. This donut is covered in a colorful array of sprinkles atop a sweet glaze, making it a festive and delightful treat that is as fun to look at as it is to eat.",
+        category: "Donut"
+    }, {
+        _id: "17",
+        name: "Margherita Pizza",
+        image: food_17,
+        price: 25,
+        description: "Our Margherita Pizza is a classic Italian favorite, featuring a simple yet delicious combination of fresh mozzarella, ripe tomatoes, and aromatic basil leaves on a thin, crispy crust.",
+        category: "Pizza"
+    }, {
+        _id: "18",
+        name: "Pepperoni Pizza",
+        image: food_18,
+        price: 25,
+        description: "Enjoy the bold flavors of our Pepperoni Pizza, topped with generous slices of spicy pepperoni over a bed of melted mozzarella cheese and savory tomato sauce.",
+        category: "Pizza"
+    }, {
+        _id: "19",
+        name: "Buffalo Chicken Pizza",
+        image: food_19,
+        price: 25,
+        description: "Spice up your pizza night with our Buffalo Chicken Pizza, topped with spicy buffalo sauce, grilled chicken, red onions, and a blend of mozzarella and cheddar cheese.",
+        category: "Pizza"
+    }, {
+        _id: "20",
+        name: "Meat Lover's Pizza:",
+        image: food_20,
+        price: 25,
+        description: "Indulge in our Meat Lover's Pizza, piled high with a hearty mix of pepperoni, sausage, ham, bacon, and ground beef. Covered with melted mozzarella cheese and our signature tomato sauce, it's a meat lover's dream.",
+        category: "Pizza"
+    }, {
+        _id: "21",
+        name: "Macaron",
+        image: food_21,
+        price: 18,
+        description: "Indulge in the delicate elegance of our French Macarons. These delightful almond-based confections come in an array of vibrant colors and exquisite flavors, sandwiched together with luxurious ganache, jam, or cream",
+        category: "Cookies"
+    }, {
+        _id: "22",
+        name: "Biscotti",
+        image: food_22,
+        price: 15,
+        description: "Treat yourself to the classic Italian delight of our Biscotti. These twice-baked, crunchy cookies are perfect for dipping into your favorite beverage.",
+        category: "Cookies"
+    }, {
+        _id: "23",
+        name: "Shortbread",
+        image: food_23,
+        price: 15,
+        description: "Enjoy the buttery goodness of our Scottish Shortbread. Made with a simple blend of butter, flour, and sugar, these delicate cookies melt in your mouth with every bite",
+        category: "Cookies"
+    }, {
+        _id: "24",
+        name: "Chocolate Cookies",
+        image: food_24,
+        price: 25,
+        description: "Indulge your sweet tooth with our delectable Chocolate Cookies. Crafted from premium chocolate and loaded with chocolate chips, these cookies offer a rich, satisfying taste experience. ",
+        category: "Cookies"
+    },
+    {
+        _id: "25",
+        name: "Penne Arrabbiata",
+        image: food_25,
+        price: 40,
+        description: "Penne Arrabbiata is a spicy and flavorful Italian pasta dish. It features al dente penne pasta served with a spicy tomato sauce flavored with garlic, red chili peppers, and tomatoes.",
+        category: "Pasta"
+    },
+    {
+        _id: "26",
+        name: "Fettuccine Alfredo",
+        image: food_26,
+        price: 45,
+        description: "Fettuccine Alfredo is a delicious and classic pasta dish made of al dente fettuccine noodles served with a rich and creamy Alfredo sauce made from butter, cream, and Parmesan cheese.",
+        category: "Pasta"
+    }, {
+        _id: "27",
+        name: "Rigatoni with Spicy Pork Ragu",
+        image: food_27,
+        price: 45,
+        description: "Indulge in the bold flavors of our Rigatoni with Spicy Pork Ragu. This hearty pasta dish features rigatoni pasta cooked al dente and tossed in a spicy pork ragu sauce made with tender chunks of pork, tomatoes, garlic, and red pepper flakes.",
+        category: "Pasta"
+    }, {
+        _id: "28",
+        name: "Macaroni Schotel",
+        image: food_28,
+        price: 40,
+        description: "Our Macaroni Schotel is a comforting and cheesy baked pasta dish that's perfect for any occasion. Tender macaroni noodles are enveloped in a creamy cheese sauce, mixed with savory ham or chicken, and baked until golden and bubbly",
+        category: "Pasta"
+    }, {
+        _id: "29",
+        name: "Sparkling Strawberry Lemonade",
+        image: food_29,
+        price: 20,
+        description: "Quench your thirst with our Sparkling Lemonade. Made with freshly squeezed lemon juice, sparkling water, and a hint of sweetness, it's the perfect refreshing beverage to enjoy alongside our delectable bakery treats.g",
+        category: "Drink"
+    }, {
+        _id: "30",
+        name: "Mojito",
+        image: food_30,
+        price: 20,
+        description: "Experience the refreshing sensation of our Mojito. This classic drink consists of white rum, soda, lime juice, sugar, and fresh mint leaves. Each sip brings a tropical freshness, perfect to accompany your indulgence in our sweet and savory bakery delights.",
+        category: "Drink"
+    }, {
+        _id: "31",
+        name: "Smoothie",
+        image: food_31,
+        price: 25,
+        description: "Indulge in our refreshing and nutritious smoothies. Made with a blend of fresh fruits such as banana, strawberry, and blueberry, combined with yogurt or orange juice, our smoothies are a delicious and healthy option to enjoy any time of the day.",
+        category: "Drink"
+    }, {
+        _id: "32",
+        name: "Iced Coffee",
+        image: food_32,
+        price: 25,
+        description: "Savor the cool and invigorating taste of our Iced Coffee. Made with chilled espresso, ice cubes, your choice of sweetener, and milk, it's the perfect pick-me-up for a hot day.",
+        category: "Drink"
+    }
+]
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here!</h2>
+        <p>Welcome to Dilan Bakery, a culinary haven for lovers of fresh pastries, bread, muffins, and donuts! Located in the heart of the city, Dilan Bakery proudly offers a wide array of baked goods, crafted with love and high-quality ingredients.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
